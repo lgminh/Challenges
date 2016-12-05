f0a6413d36a578f95f2e039a077e7553a17fff3b
DANIEL-LOC\USER <lgminh190293@gmail.com>
2016-12-05 22:23:53 +0700

    first commit

---
+++ Medium/number_game.py
@@ -0,0 +1,42 @@
+import numpy, pandas, unicodecsv
+
+def number_game(number, n):
+    def backtracking(number, subset, x, j):
+        n = ''.join([str(number[i]) for i in xrange(len(number)) if subset[i] == 1])
+        if n == '' or n == '0':
+            return -1
+        else:
+            if x == 1:
+                return x
+            if int(n) % x == 0:
+                # check if current number can be divided by x
+                for i in xrange(len(number)):
+                    if i != j and subset[i] == 1:
+                        subset[i] = 0
+                        value = backtracking(number, subset, x - 1, i)
+                        subset[i] = 1
+                        if value == 1:
+                            return value
+                return x
+            else:
+                return x
+
+    subset = [1 for i in xrange(len(number))]
+
+    res = n - 1
+    for i in xrange(len(number)):
+        subset[i] = 0
+        value = backtracking(number, subset, n, i)
+        subset[i] = 1
+        if value < res:
+            res = value
+    return n - res
+
+
+def main():
+    #print number_game("102045", 4)
+    print number_game("5555", 3)
+    print number_game("88",2)
+    print number_game("912056", 3)
+if __name__ == '__main__':
+    main()
